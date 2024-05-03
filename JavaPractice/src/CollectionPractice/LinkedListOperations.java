d201f1c0614c7fd2405ae76cb705d13f935a940f
Abhishek Pardhiya <LENOVO@192.168.1.38>
2024-05-03 17:19:49 +0530

    linked list operation added

---
+++ JavaPractice/src/CollectionPractice/LinkedListOperations.java
@@ -0,0 +1,57 @@
+package CollectionPractice;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+
+public class LinkedListOperations {
+	
+	public static void main(String[] args) {
+
+		// create an Linked List using List interface
+		List<String> list = new LinkedList<>();
+
+		// Create an list using LInked List class
+		LinkedList<String> linkedList = new LinkedList<>();
+		
+		
+		//adding the element using default add() method that adds the element at the end of the list
+		linkedList.add("apple");
+		linkedList.add("banana");
+		System.out.println(linkedList);
+		
+		//adding an element using addLast() method
+		linkedList.addLast("cherry");
+		System.out.println("added the element to the last : "+linkedList );
+		
+		//adding an element to the first of linked list
+		linkedList.addFirst("Guava");
+		System.out.println("added the element to the first : "+linkedList );
+		
+		//adding element at the specific index
+		linkedList.add(2,"Mango");
+		System.out.println("added the element at the index 2 : "+linkedList );
+		
+		//deleting an element at specific index
+		linkedList.remove(0);
+		System.out.println("deleted the element at the index 0 : "+linkedList );
+		
+		//checking if the linked list contains a specific element 
+		System.out.println(linkedList.contains("Mango"));
+		
+		for(String val:linkedList) {
+			System.out.println( "Traversing :"+val);
+		}
+		System.out.println("-------------------------------------------------");
+		
+		Iterator<String> itr = linkedList.iterator();
+		while(itr.hasNext()) {
+			System.out.println("Using Iterator:- "+itr.next());
+		}
+		
+		
+
+		
+	}
+
+}
